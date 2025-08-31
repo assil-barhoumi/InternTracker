04bb1ab404bce31cdf51073320d6d477c9329a48
acyyl <assil.barhoumi@isimg.tn>
2025-08-31 11:54:48 +0100

    Add user authentication and homepage

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),  
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'), 
+
 ]
