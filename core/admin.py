cb4a751571214aa67ae23b57245e0a8af9ba792f
acyyl <assil.barhoumi@isimg.tn>
2025-08-31 10:06:06 +0100

    Configure admin for core models with restricted permissions

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import InternshipOffer, Intern, Interview
 
-# Register your models here.
+@admin.register(InternshipOffer)
+class InternshipOfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'department', 'duration')
+
+@admin.register(Intern)
+class InternAdmin(admin.ModelAdmin):
+    list_display = ('user', 'status')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+@admin.register(Interview)
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ('intern', 'internship_offer', 'date_time', 'completed')
